1e1886d458188d3cc053596ebfaa9e73bdd69030
cliffckerr <questionably@gmail.com>
2018-08-17 15:46:22 +1000

    removing layer

---
+++ quickserver.py
@@ -0,0 +1,208 @@
+"""
+A simple server used to show mpld3 images -- based on _server in the mpld3 package.
+
+Version: 2015dec29
+"""
+
+import threading
+import webbrowser
+import socket
+import itertools
+import random
+import numpy as np
+from collections import OrderedDict
+import sciris.core as sc
+try:    import BaseHTTPServer as server # Python 2.x
+except: from http import server # Python 3.x
+
+def normalize_obj(obj):
+    """
+    This is the main conversion function for Python data-structures into
+    JSON-compatible data structures.
+
+    Use this as much as possible to guard against data corruption!
+
+    Args:
+        obj: almost any kind of data structure that is a combination
+            of list, numpy.ndarray, odicts etc
+
+    Returns:
+        A converted dict/list/value that should be JSON compatible
+    """
+
+    if isinstance(obj, list) or isinstance(obj, tuple):
+        return [normalize_obj(p) for p in list(obj)]
+    
+    if isinstance(obj, np.ndarray):
+        if obj.shape: # Handle most cases, incluing e.g. array([5])
+            return [normalize_obj(p) for p in list(obj)]
+        else: # Handle the special case of e.g. array(5)
+            return [normalize_obj(p) for p in list(np.array([obj]))]
+
+    if isinstance(obj, dict):
+        return {str(k): normalize_obj(v) for k, v in obj.items()}
+
+    if isinstance(obj, sc.odict):
+        result = OrderedDict()
+        for k, v in obj.items():
+            result[str(k)] = normalize_obj(v)
+        return result
+
+    if isinstance(obj, np.bool_):
+        return bool(obj)
+
+    if isinstance(obj, float):
+        if np.isnan(obj):
+            return None
+
+    if isinstance(obj, np.float64):
+        if np.isnan(obj):
+            return None
+        else:
+            return float(obj)
+
+    if isinstance(obj, unicode):
+        try:    string = str(obj) # Try to convert it to ascii
+        except: string = obj # Give up and use original
+        return string
+
+    if isinstance(obj, set):
+        return list(obj)
+
+    return obj
+
+def generate_handler(html, files=None):
+    if files is None:
+        files = {}
+
+    class MyHandler(server.BaseHTTPRequestHandler):
+        def do_GET(self):
+            """Respond to a GET request."""
+            if self.path == '/':
+                self.send_response(200)
+                self.send_header("Content-type", "text/html")
+                self.end_headers()
+                self.wfile.write(html.encode())
+            elif self.path in files:
+                content_type, content = files[self.path]
+                self.send_response(200)
+                self.send_header("Content-type", content_type)
+                self.end_headers()
+                self.wfile.write(content.encode())
+            else:
+                self.send_error(404)
+
+    return MyHandler
+
+
+def find_open_port(ip, port, n=50):
+    """Find an open port near the specified port"""
+    ports = itertools.chain((port + i for i in range(n)), (port + random.randint(-2 * n, 2 * n)))
+    for port in ports:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        result = s.connect_ex((ip, port))
+        s.close()
+        if result != 0: return port
+    raise ValueError("No open ports found")
+
+
+def serve(html, ip='127.0.0.1', port=8888, n_retries=50):
+    """Start a server serving the given HTML, and open a browser
+
+    Example:
+       html = '<b>Hello, world!</b>'
+       import optima as op
+       op._webserver.serve(html)
+
+    Parameters
+    ----------
+    html : string
+        HTML to serve
+    ip : string (default = '127.0.0.1')
+        ip address at which the HTML will be served.
+    port : int (default = 8888)
+        the port at which to serve the HTML
+    n_retries : int (default = 50)
+        the number of nearby ports to search if the specified port is in use.
+    """
+    port = find_open_port(ip, port, n_retries)
+    Handler = generate_handler(html)
+
+    # Create server
+    srvr = server.HTTPServer((ip, port), Handler)
+
+    # Use a thread to open a web browser pointing to the server
+    try: browser = webbrowser.get('google-chrome') # CK: Try google Chrome first
+    except: browser = webbrowser.get() # If not, just use whatever
+    b = lambda: browser.open('http://{0}:{1}'.format(ip, port))
+    threading.Thread(target=b).start()
+
+    # CK: don't serve forever, just create it once
+    srvr.handle_request()
+
+
+def browser(figs=None, doserve=True, jquery_url=None, d3_url=None, mpld3_url=None):
+    ''' 
+    Create an MPLD3 GUI and display in the browser.
+    
+    Usage example:
+        import pylab as pl
+        figs = []
+        for n in [10, 50]:
+            fig = pl.figure()
+            pl.plot(pl.rand(n), pl.rand(n))
+            figs.append(fig)
+        qs.browser(figs)
+    
+    figs can be a single figure or a list of figures.
+    
+    With doserve=True, launch a web server. Otherwise, return the HTML representation of the figures.
+    
+    Version: 2018jul24
+    '''
+    import mpld3 # Only import this if needed, since might not always be available
+    import json
+    legacy = False
+
+    ## Specify the div style, and create the HTML template we'll add the data to -- WARNING, library paths are hardcoded!
+    divstyle = "float: left"
+    if legacy:
+        if jquery_url is None: jquery_url = 'https://code.jquery.com/jquery-1.11.3.min.js'
+        if d3_url     is None: d3_url     = 'https://mpld3.github.io/js/d3.v3.min.js'
+        if mpld3_url  is None: mpld3_url  = 'https://mpld3.github.io/js/mpld3.v0.3git.js'
+    else:
+        if jquery_url is None: jquery_url = 'https://code.jquery.com/jquery-1.11.3.min.js'
+        if d3_url     is None: d3_url     = 'http://thekerrlab.com/tmp/d3.v5.min.js'
+        if mpld3_url  is None: mpld3_url  = 'http://thekerrlab.com/tmp/mpld3.v0.4.0.min.js'
+    html = '''
+    <html>
+    <head><script src="%s"></script></head>
+    <body>
+    !MAKE DIVS!
+    <script>
+    function mpld3_load_lib(url, callback){var s = document.createElement('script'); s.src = url; s.async = true; s.onreadystatechange = s.onload = callback; s.onerror = function(){console.warn("failed to load library " + url);}; document.getElementsByTagName("head")[0].appendChild(s)} mpld3_load_lib("%s", function(){mpld3_load_lib("%s", function(){
+    !DRAW FIGURES!
+    })});
+    </script>
+    </body></html>
+    ''' % (jquery_url, d3_url, mpld3_url)
+    
+    ## Create the figures to plot
+    jsons = [] # List for storing the converted JSONs
+    figs = sc.promotetolist(figs)
+    nfigs = len(figs) # Figure out how many plots there are
+    for f in range(nfigs): # Loop over each plot
+        jsons.append(str(json.dumps(normalize_obj(mpld3.fig_to_dict(figs[f]))))) # Save to JSON
+    
+    ## Create div and JSON strings to replace the placeholers above
+    divstr = ''
+    jsonstr = ''
+    for f in range(nfigs):
+        divstr += '<div style="%s" id="fig%i" class="fig"></div>\n' % (divstyle, f) # Add div information: key is unique ID for each figure
+        jsonstr += 'mpld3.draw_figure("fig%i", %s);\n' % (f, jsons[f]) # Add the JSON representation of each figure -- THIS IS KEY!
+    html = html.replace('!MAKE DIVS!',divstr) # Populate div information
+    html = html.replace('!DRAW FIGURES!',jsonstr) # Populate figure information
+    
+    ## Launch a server or return the HTML representation
+    if doserve: serve(html)
+    else:       return html
---
+++ scirisobjects.py
@@ -0,0 +1,378 @@
+"""
+scirisobjects.py -- classes for Sciris objects which are generally managed
+    
+Last update: 7/12/18 (gchadder3)
+"""
+
+#
+# Imports
+#
+
+from . import datastore as ds
+from ..corelib import utils as ut
+
+#
+# Classes
+#
+
+class ScirisObject(object):
+    """
+    A general Sciris object (base class for all such objects).  Objects of this 
+    type are meant to be storable using Sciris' DataStore persistence 
+    functionality.
+    
+    Methods:
+        __init__(uid: UUID [None], type_prefix: str ['obj'], 
+            file_suffix: str ['.obj'], instance_label: str ['']): 
+            void -- constructor
+        in_data_store(): bool -- is the object stored in the DataStore?
+        load_from_copy(other_obj): void -- assuming other_obj is another 
+            object of our type, copy its contents to us
+        add_to_data_store(): void -- add ourselves to the Sciris DataStore 
+            (theDataStore object)
+        load_copy_from_data_store(uid: UUID [None]): ScirisObject -- return 
+            the ScirisObject matching a UUID (usually ourselves in UUID is 
+            None)
+        load_from_data_store(self, uid=None): void -- overwrite our current 
+            state with what's in the DataStore (usually from our stored 
+            object, but you can pass in another UUID also to copy from another 
+            stored object)
+        update_data_store(): void -- update the DataStore representation from 
+            our current state
+        delete_from_data_store(): void -- delete ourselves from the DataStore        
+        show(): void -- print the contents of the object       
+        get_user_front_end_repr(): dict -- get a JSON-friendly dictionary 
+            representation of the object state the front-end uses for non-
+            admin purposes
+        get_admin_front_end_repr(): dict -- get a JSON-friendly dictionary
+            representation of the object state the front-end uses for admin
+            purposes
+                    
+    Attributes:
+        uid (UUID) -- the unique ID for the user (uuid Python library-related)
+        type_prefix (str) -- a prefix that gets added to the UUID to give either 
+            a file name or a Redis key
+        file_suffix (str) -- a suffix that gets added to files
+        instance_label (str) -- a name of the object which should at least be 
+            unique across other handles of the save type_prefix
+            
+    Usage:
+        >>> obj = ScirisObject(uuid.UUID('12345678123456781234567812345678'))                      
+    """
+    
+    def  __init__(self, uid=None, type_prefix='obj', file_suffix='.obj', 
+        instance_label=''):       
+        # Get a valid UUID from what is passed in, or if None is passed in, 
+        # get a new ID.
+        self.uid = ut.uuid(uid) 
+            
+        # Set the other variables that might be used with DataStore.
+        self.type_prefix = type_prefix
+        self.file_suffix = file_suffix
+        self.instance_label = instance_label
+        
+    def in_data_store(self):
+        return (ds.data_store.retrieve(self.uid) is not None)
+    
+    def load_from_copy(self, other_obj):
+        if type(other_obj) == type(self):
+            self.uid = other_obj.uid
+            self.type_prefix = other_obj.type_prefix
+            self.instance_label = other_obj.instance_label
+            
+    def add_to_data_store(self):
+        # Check to see if the object is already in the DataStore, and give an 
+        # error if so.
+        if self.in_data_store():
+            print('Error: Object ''%s'' is already in DataStore.' % self.uid.hex)
+            return
+        
+        # Add our representation to the DataStore.
+        ds.data_store.add(self, self.uid, self.type_prefix, self.file_suffix, 
+            self.instance_label)
+        
+    def load_copy_from_data_store(self, uid=None):
+        if uid is None:
+            if not self.in_data_store():
+                print('Error: Object ''%s'' is not in DataStore.' % self.uid.hex)
+                return None
+            else:
+                return ds.data_store.retrieve(self.uid)
+        else:
+            return ds.data_store.retrieve(uid)
+            
+    def load_from_data_store(self, uid=None):
+        # Get a copy from the DataStore.
+        copy_from_store = self.load_copy_from_data_store(uid)
+        
+        # Copy the internal information over from the copy to ourselves.
+        self.load_from_copy(copy_from_store)
+        
+    def update_data_store(self):
+        # Give an error if the object is not in the DataStore.
+        if not self.in_data_store():
+            print('Error: Object ''%s'' is not in DataStore.' % self.uid.hex)
+            return
+        
+        # Update our DataStore representation with our current state. 
+        ds.data_store.update(self.uid, self) 
+        
+    def delete_from_data_store(self):
+        # Give an error if the object is not in the DataStore.
+        if not self.in_data_store():
+            print('Error: Object ''%s'' is not in DataStore.' % self.uid.hex)
+            return
+        
+        # Delete ourselves from the DataStore.
+        ds.data_store.delete(self.uid)
+    
+    def show(self):
+        print('--------------------------------------------')
+        print('UUID: %s' % self.uid.hex)
+        print('Type Prefix: %s' % self.type_prefix)
+        print('File Suffix: %s' % self.file_suffix)
+        print('Instance Label: %s' % self.instance_label)
+        in_data_store = self.in_data_store()
+        if in_data_store:
+            print('In DataStore?: Yes')
+        else:
+            print('In DataStore?: No')
+        #print '--------------------------------------------'
+        
+    def get_user_front_end_repr(self):
+        obj_info = {
+            'scirisobject': {
+                'instance_label': self.instance_label                
+            }
+        }
+        return obj_info
+    
+    def get_admin_front_end_repr(self):
+        obj_info = {
+            'scirisobject': {
+                'UID': self.uid.hex, 
+                'type_prefix': self.type_prefix, 
+                'file_suffix': self.file_suffix, 
+                'instance_label': self.instance_label                
+            }
+        }
+        return obj_info 
+          
+class ScirisCollection(ScirisObject):
+    """
+    A collection of ScirisObjects (stored in a Python dict obj_dict).
+    
+    Methods:
+        __init__(uid: UUID [None], type_prefix: str ['collection'], 
+            file_suffix: str ['.scl'], instance_label: str [''], 
+            objs_within_dict: bool [False]): void -- constructor        
+        load_from_copy(other_obj): void -- assuming other_obj is another 
+            object of our type, copy its contents to us (calls the 
+            ScirisObject superclass version of this method also)
+        get_object_by_uid(uid: UUID): ScirisObject -- get a ScirisObject out of 
+            the collection by the UUID passed in
+        get_all_objects(): list of ScirisObjects -- get all of the ScirisObjects 
+           and put them in a list
+        add_object(obj: ScirisObject): void -- add a ScirisObject to the 
+            collection
+        update_object(obj: ScirisObject): void -- update a ScirisObject 
+            passed in to the collection
+        delete_object_by_uid(uid: UUID): void -- delete the ScirisObject 
+            indexed by the UUID from the collection
+        delete_all_objects(): void -- delete all ScirisObjects from the 
+            collection
+        show(): void -- print the contents of the collection, including the 
+            object information as well as the objects
+                    
+    Attributes:
+        obj_dict (dict) -- the Python dictionary holding the ScirisObjects
+        objs_within_coll (bool) -- are the objects themselves within the 
+            collection? If they are not, they are kept as separate data store 
+            entries and handle_dict is used.
+        ds_uuid_set (set) -- the Python set holding UUIDs to DataStore 
+            entries for the ScirisObjects (used only if objs_within_coll is False)
+        
+    Usage:
+        >>> objs = ScirisCollection(uuid.UUID('12345678123456781234567812345678'))                      
+    """
+    
+    def __init__(self, uid, type_prefix='collection', 
+        file_suffix='.scl', instance_label='', objs_within_coll=False):
+        # Set superclass parameters.
+        super(ScirisCollection, self).__init__(uid, type_prefix, 
+             file_suffix, instance_label)
+        
+        # Create a Python dict to hold the ScirisObjects.
+        self.obj_dict = {}
+        
+        # Set the flag for whether the objects are stored within the collection 
+        # itself (or outside in separate data store entries).
+        self.objs_within_coll = objs_within_coll
+        
+        # Set the datastore UUID set to empty.
+        self.ds_uuid_set = set()
+        
+    def load_from_copy(self, other_obj):
+        if type(other_obj) == type(self):
+            # Do the superclass copying.
+            super(ScirisCollection, self).load_from_copy(other_obj)
+            
+            # Copy other items specific to this class.
+            self.obj_dict = other_obj.obj_dict
+            self.objs_within_coll = other_obj.objs_within_coll
+            self.ds_uuid_set = other_obj.ds_uuid_set
+            
+    def get_object_by_uid(self, uid):
+        # Make sure the argument is a valid UUID, converting a hex text to a
+        # UUID object, if needed.
+        valid_uid = ut.uuid(uid)
+        
+        # If we have a valid UUID...
+        if valid_uid is not None:
+            # If we have the objects within the collection itself...
+            if self.objs_within_coll:
+                # Return the object.
+                return self.obj_dict.get(valid_uid, None)
+            
+            # Otherwise, if the UUID is in the set...
+            elif valid_uid in self.ds_uuid_set:
+                # Return the object that is in the global DataStore object.
+                return ds.data_store.retrieve(valid_uid)
+            
+            # Otherwise (no match)...
+            else:
+                return None
+        else:
+            return None
+        
+    def get_all_objects(self):
+        if self.objs_within_coll:
+            return [self.obj_dict[key] for key in self.obj_dict]
+        else:
+            return [ds.data_store.retrieve(uid) for uid in self.ds_uuid_set]
+    
+    def add_object(self, obj):
+        # If we are storing things inside the obj_dict...
+        if self.objs_within_coll:        
+            # Add the object to the hash table, keyed by the UID.
+            self.obj_dict[obj.uid] = obj
+            
+        # Otherwise, we are using the UUID set...
+        else:
+            # Put the UUID in the set.
+            self.ds_uuid_set.add(obj.uid)
+            
+            # If the object is not actually in the DataStore, put it there.
+            if not obj.in_data_store():
+                obj.add_to_data_store()
+        
+        # Update our DataStore representation if we are there. 
+        if self.in_data_store():
+            self.update_data_store()
+            
+    def update_object(self, obj):
+        # If we are storing things inside the obj_dict...
+        if self.objs_within_coll:           
+            # Do the same behavior as add_object().
+            self.add_object(obj)
+            
+        # Otherwise, we are using the UUID set...
+        else:
+            # Update the object in the global DataStore object.
+            ds.data_store.update(obj.uid, obj)
+            
+    def delete_object_by_uid(self, uid):
+        # Make sure the argument is a valid UUID, converting a hex text to a
+        # UUID object, if needed.        
+        valid_uid = ut.uuid(uid)
+        
+        # If we have a valid UUID...
+        if valid_uid is not None:
+            # Default to not needing to update the data store.
+            need_to_update = False
+            
+            # If we are storing things inside the obj_dict...
+            if self.objs_within_coll:             
+                # Get the object pointed to by the UID.
+                obj = self.obj_dict[valid_uid]
+                
+                # If a match is found...
+                if obj is not None:
+                    # Remove entries from obj_dict.
+                    del self.obj_dict[valid_uid]
+                    
+                    # Set to update the data store
+                    need_to_update = True
+                    
+            # Otherwise, we are using the UUID set...
+            else:
+                # If the UUID is in the set...
+                if valid_uid in self.ds_uuid_set:
+                    # Remove the UUID from the set.
+                    self.ds_uuid_set.remove(valid_uid)
+                    
+                    # Delete the object in the global DataStore object.
+                    ds.data_store.delete(valid_uid)
+                    
+                    # Set to update the data store
+                    need_to_update = True                    
+            
+            # Update our DataStore representation if we are there. 
+            if need_to_update and self.in_data_store():
+                self.update_data_store()
+    
+    def delete_all_objects(self):
+        # If we are storing things inside the obj_dict...
+        if self.objs_within_coll:          
+            # Reset the Python dicts.
+            self.obj_dict = {}
+            
+        # Otherwise, we are using the UUID set...
+        else:
+            # For each item in the set...
+            for uid in self.ds_uuid_set:
+                # Delete the object with that UID in the DataStore.
+                ds.data_store.delete(uid)
+                
+            # Clear the UUID set.
+            self.ds_uuid_set.clear()
+         
+        # Update our DataStore representation if we are there. 
+        if self.in_data_store():
+            self.update_data_store()  
+        
+    def show(self):
+        # Show superclass attributes.
+        super(ScirisCollection, self).show()  
+
+        if self.objs_within_coll:
+            print('Objects stored within dict?: Yes')
+        else:
+            print('Objects stored within dict?: No')
+            
+        print('---------------------')
+        print('Contents')
+        print('---------------------')
+        
+        # If we are storing things inside the obj_dict...
+        if self.objs_within_coll:
+            # For each key in the dictionary...
+            for key in self.obj_dict:
+                # Get the object pointed to.
+                obj = self.obj_dict[key]
+                
+                # Separator line.
+                #print '--------------------------------------------'
+                
+                # Show the handle contents.
+                obj.show()
+                
+        # Otherwise, we are using the UUID set.
+        else:
+            # For each item in the set...
+            for uid in self.ds_uuid_set:
+                # Show the object with that UID in the DataStore.
+                ds.data_store.retrieve(uid).show()
+            
+        # Separator line.
+        print('--------------------------------------------')
