ff9b12807fdd864072452c114fe48e4a46f3e0be
cliffckerr <questionably@gmail.com>
2018-10-24 11:48:18 +1100

    rename

--- scirisweb/__init__.py
+++ scirisweb/__init__.py
@@ -9,13 +9,13 @@ Version: 2018oct24
 '''
 
 # Import everything
-from .sc_version   import * # analysis:ignore
-from .sc_rpcs      import * # analysis:ignore
-from .sc_users     import * # analysis:ignore
-from .sc_tasks     import * # analysis:ignore
-from .sc_datastore import * # analysis:ignore
-from .sc_app       import * # analysis:ignore
-from .sc_server    import * # analysis:ignore
+from .sw_version   import * # analysis:ignore
+from .sw_rpcs      import * # analysis:ignore
+from .sw_users     import * # analysis:ignore
+from .sw_tasks     import * # analysis:ignore
+from .sw_datastore import * # analysis:ignore
+from .sw_app       import * # analysis:ignore
+from .sw_server    import * # analysis:ignore
 
 # Print the license
 print(__license__)
