299ec3dc9c43a4903575f7193e0040a4d974b311
cliffckerr <questionably@gmail.com>
2018-11-24 18:01:18 +1100

    improve setup.py

--- scirisweb/sw_version.py
+++ scirisweb/sw_version.py
@@ -1,5 +1,5 @@
 __all__ = ['__version__', '__versiondate__', '__license__']
 
-__version__      = '0.11.2'
+__version__      = '0.11.3'
 __versiondate__  = '2018-11-23'
 __license__      = 'ScirisWeb %s (%s) -- (c) Sciris.org' % (__version__, __versiondate__)
--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from setuptools import setup, find_packages
 
-with open("scirisweb/sw_version.py", "r") as f:
-    version_file = {}
-    exec(f.read(), version_file)
-    version = version_file["__version__"]
+# Get version information
+versionfile = 'scirisweb/sw_version.py'
+with open(versionfile, 'r') as f:
+    versiondict = {}
+    exec(f.read(), versiondict)
+    version = versiondict['__version__']
 
 CLASSIFIERS = [
     'Environment :: Console',
@@ -16,8 +17,9 @@ CLASSIFIERS = [
     'Operating System :: OS Independent',
     'Programming Language :: Python',
     'Topic :: Software Development :: Libraries :: Python Modules',
-    'Development Status :: 3 - Alpha',
+    'Development Status :: 4 - Beta',
     'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3.7',
 ]
 
 setup(
@@ -26,8 +28,8 @@ setup(
     author='ScirisOrg',
     author_email='info@sciris.org',
     description='Scientific webapps for Python',
-    url='http://github.com/optimamodel/sciris',
-    keywords=['scientific','webapp', 'framework'],
+    url='http://github.com/sciris/scirisweb',
+    keywords=['scientific', 'webapp', 'framework'],
     platforms=['OS Independent'],
     classifiers=CLASSIFIERS,
     packages=find_packages(),
