31fffdc75ac8cb23ef625d2f59d843ac92295dc1
cliffckerr <questionably@gmail.com>
2018-11-24 18:24:54 +1100

    incremented version

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open(versionfile, 'r') as f:
 CLASSIFIERS = [
     'Environment :: Console',
     'Intended Audience :: Science/Research',
-    'License :: OSI Approved :: GPLv3',
+    'License :: OSI Approved :: MIT License',
     'Operating System :: OS Independent',
     'Programming Language :: Python',
     'Topic :: Software Development :: Libraries :: Python Modules',
