e75a6e753d888416e75ba3046271c39d1daee8a8
gchadder3 <gchadder3@gmail.com>
2018-11-29 11:31:26 +1100

    Have hellograph2 working, but only after axing robustjsonify call

--- examples/hellograph2/app.py
+++ examples/hellograph2/app.py
@@ -4,8 +4,8 @@ import sciris as sc
 import scirisweb as sw
 
 # Globals
-RPC_dict = {} # Dictionary to hold all of the registered RPCs in this module.
-RPC = sw.RPCwrapper(RPC_dict) # RPC registration decorator factory created using call to make_RPC().
+# RPC_dict = {} # Dictionary to hold all of the registered RPCs in this module.
+# RPC = sw.RPCwrapper(RPC_dict) # RPC registration decorator factory created using call to make_RPC().
 
 runserver = True # Choose to run in the frontend or backend
 
@@ -14,7 +14,7 @@ app = sw.ScirisApp(__name__, name="HelloGraph")
 
 # Define the API
 @app.route('/showgraph')
-def showgraph(n=1000):
+def showgraph(n=100):
     
     # Make graph
     fig = pl.figure()
@@ -26,10 +26,11 @@ def showgraph(n=1000):
     
     # Convert to FE
     graphjson = sw.mpld3ify(fig)  # Convert to dict
+    print(graphjson)
     return graphjson  # Return the JSON representation of the Matplotlib figure
 
 # Define the RPC
-@RPC()
+@app.register_RPC()
 def showgraph2(n=1000):
     # Make graph
     fig = pl.figure()
@@ -41,6 +42,7 @@ def showgraph2(n=1000):
 
     # Convert to FE
     graphjson = sw.mpld3ify(fig)  # Convert to dict
+    print(graphjson)
     return graphjson  # Return the JSON representation of the Matplotlib figure
 
 # Run the server
