ae4ed08dbc6d043cefe77133966bc41fc8b24fe2
cliffckerr <questionably@gmail.com>
2019-05-23 00:41:37 +0100

    remove debugging comments

--- scirisweb/__init__.py
+++ scirisweb/__init__.py
@@ -9,22 +9,13 @@ Version: 2018oct24
 '''
 
 # Import everything
-import matplotlib as m
-print(m.get_backend())
 from .sw_version   import * # analysis:ignore
-print(m.get_backend())
 from .sw_rpcs      import * # analysis:ignore
-print(m.get_backend())
 from .sw_users     import * # analysis:ignore
-print(m.get_backend())
 from .sw_tasks     import * # analysis:ignore
-print(m.get_backend())
 from .sw_datastore import * # analysis:ignore
-print(m.get_backend())
 from .sw_app       import * # analysis:ignore
-print(m.get_backend())
 from .sw_server    import * # analysis:ignore
-print(m.get_backend())
 
 # Print the license
 print(__license__)
