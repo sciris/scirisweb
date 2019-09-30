93580ea58f7d628d56f23084516bdba176657863
Romesh Abeysuriya <romesh.abey@gmail.com>
2019-09-30 10:06:14 +0800

    Version bump (major version according to semver)

--- scirisweb/sw_version.py
+++ scirisweb/sw_version.py
@@ -1,5 +1,5 @@
 __all__ = ['__version__', '__versiondate__', '__license__']
 
-__version__      = '0.12.10'
-__versiondate__  = '2019-06-10'
+__version__      = '1.0.0'
+__versiondate__  = '2019-09-30'
 __license__      = 'ScirisWeb %s (%s) -- (c) Sciris.org' % (__version__, __versiondate__)
