f0e6c7e33db66b1831814f80ecd434afb65cc09f
cliffckerr <questionably@gmail.com>
2021-02-08 20:46:36 -0800

    more tidying

--- examples/react/api/api.py
+++ examples/react/api/api.py
@@ -1,21 +1,25 @@
-import time
-# from flask import Flask
+'''
+Web server. Run with:
+    python api.py
+'''
 
 import pylab as pl
 import sciris as sc
 import scirisweb as sw
 
-# app = Flask(__name__)
+autoreload = True
 app = sw.ScirisApp(__name__, name="React example", server_port=5000)
 
-# @app.register_RPC()
+
 @app.route('/get_time')
 def get_time():
-    return {'time': time.time()}
+    ''' Can still define standard Flask routes '''
+    return {'time': sc.getdate()}
 
-# @app.route('/showgraph')
+
 @app.register_RPC()
 def showgraph(n=1000):
+    ''' Or can define Sciris remote procedure calls (RPCs) '''
 
     # Make graph
     fig = pl.figure()
@@ -24,6 +28,7 @@ def showgraph(n=1000):
     ydata = pl.randn(n)
     colors = sc.vectocolor(pl.sqrt(xdata**2+ydata**2))
     ax.scatter(xdata, ydata, c=colors)
+    ax.patch.set_alpha(0.0) # Go fancy with transparent background
 
     # Convert to FE
     graphjson = sw.mpld3ify(fig, jsonify=False)  # Convert to dict
@@ -31,5 +36,4 @@ def showgraph(n=1000):
 
 
 if __name__ == "__main__":
-
-    app.run(autoreload=1)
+    app.run(autoreload=autoreload)
