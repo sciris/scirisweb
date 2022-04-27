ed68b7353de6615debe2dfa7e4c927d42adcd0e3
Paula Sanz-Leon <paula.sanz-leon@qimrberghofer.edu.au>
2022-04-27 19:16:08 +1000

    Use sphinx markup in serve()

--- scirisweb/sw_server.py
+++ scirisweb/sw_server.py
@@ -56,21 +56,19 @@ def find_open_port(ip, port, n=50):
 def serve(html, ip='127.0.0.1', port=8888, n_retries=50):
     """Start a server serving the given HTML, and open a browser
 
+    Parameters
+    ----------
+    :param str html: HTML to serve
+    :param str ip: ip address at which the HTML will be served (default = '127.0.0.1').
+    :param int port: the port at which to serve the HTML (default = 8888).
+    :param int n_retries: the number of nearby ports to search if the specified port is in use (default = 50).
+    :return:
+    
     Example:
        html = '<b>Hello, world!</b>'
        import scirisweb as sw
        sw.serve(html)
 
-    Parameters
-    ----------
-    html : string
-        HTML to serve
-    ip : string (default = '127.0.0.1')
-        ip address at which the HTML will be served.
-    port : int (default = 8888)
-        the port at which to serve the HTML
-    n_retries : int (default = 50)
-        the number of nearby ports to search if the specified port is in use.
     """
     port = find_open_port(ip, port, n_retries)
     Handler = generate_handler(html)
@@ -119,8 +117,6 @@ def browser(figs=None, doserve=True, legacy=False, jquery_url=None, d3_url=None,
     Create an MPLD3 GUI and display in the browser. 
      
     Document me
-
-    Parameters: 
 
     :param figs: figs to be displayed in the browser.
     :type figs: figure or list
