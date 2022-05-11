2dcb6f947667490a37e35bf760e086ebeee0d976
Paula Sanz-Leon <paula.sanz-leon@qimrberghofer.edu.au>
2022-05-11 14:53:12 +1000

    Save users to datastore

--- tests/test_users.py
+++ tests/test_users.py
@@ -9,7 +9,15 @@ import scirisweb as sw
 
 
 
-def test_new_user():
+@pytest.fixture(scope='module')
+def make_ds():
+    db_folder = 'temp_test_datastore_users'
+    db_url = f'file://{db_folder}/'
+    ds = sw.make_datastore(db_url)
+    return ds
+
+
+def test_new_user(ds):
     """
     Create a new User and then check email, and that password is encrypted
     """
@@ -17,6 +25,13 @@ def test_new_user():
     assert user.email == 'scirisweb@gremlinmail.com'
     assert user.password != user.raw_password
     assert user.is_admin == False
+
+    # Save user to datastore
+    ds.saveuser(user)
+    # Load user to datastore
+    loaded_user = ds.loaduser(username='gremlin')
+    assert user.username == loaded_user.username
+
 
 if __name__ == '__main__':
     # Test user stuff
