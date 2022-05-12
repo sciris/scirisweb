b4dd4a624d788da64a424b757bf069d0d8137f98
Paula Sanz-Leon <paula.sanz-leon@qimrberghofer.edu.au>
2022-05-12 22:18:18 +1000

    Test basic app creation functinalit

--- tests/test_app.py
+++ tests/test_app.py
@@ -26,11 +26,36 @@ def test_update_config(app):
     # Check that updates were made
     assert app.config['SERVER_PORT'] == 8888
 
+
 def test_defaults():
+
     app = sw.ScirisApp(__name__, config=sw.TestingUsersAppConfig())
     assert app.name == 'default'
 
     app = sw.ScirisApp(__name__, config=sw.Config())
     assert isinstance(app.datastore, sw.DataDir)
+
+
+def test_logfile():
+
+    logfile = 'temp_logfile.log'
+    app = sw.ScirisApp(__name__, config=sw.Config(), logfile=logfile)
+    os.remove(logfile)
+
+    ds = sw.DataDir()
+    with pytest.raises(Exception) as e:
+        app = sw.ScirisApp(__name__, config=sw.Config(), logfile=ds.tempdir_obj.name)
+
+
+def test_robustjsonify(app):
+
+    info = {"data1": "Hello world",
+            "data2": "Good morning",
+            "data3": "See you tomorrow"}
+
+    with app.flask_app.app_context():
+        response = sw.robustjsonify(info)
+        assert response.status_code == 200
+        assert response.is_json == True
 
 
