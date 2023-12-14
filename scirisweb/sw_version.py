f9cf89256356cad6ed930aad4f42a9cee511e1ee
Cliff Kerr <questionably@gmail.com>
2023-12-13 17:20:20 -0800

    updated changelog

--- scirisweb/sw_version.py
+++ scirisweb/sw_version.py
@@ -1,5 +1,5 @@
 __all__ = ['__version__', '__versiondate__', '__license__']
 
 __version__      = '1.0.0'
-__versiondate__  = '2023-12-12'
+__versiondate__  = '2023-12-13'
 __license__      = 'ScirisWeb %s (%s) -- (c) Sciris.org' % (__version__, __versiondate__)
